e7ceeff4eac48babf54c2fe0a93cae747b17d348
Chaiwat <mynameistecs51@gmail.com>
2016-08-03 21:55:38 +0700

    rename เป็นตัวใหญ่

--- application/views/teacher/header.php
+++ application/views/teacher/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" class="no-js">
 <head>
-	<title>ระบบสารสนเทศนักศึกษา</title>
+	<title>ระบบสารสนเทศอาจารย์</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
